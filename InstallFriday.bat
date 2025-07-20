e1d1d9c3e1f232b22420e0ce3977c68730bfe1a3
Becelydada52 <accauntontv52@gmail.com>
2025-07-20 12:18:53 +0300

    Delete InstallFriday.bat

--- InstallFriday.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-:: Установка необходимых библиотек
-pip install speechrecognition pycaw pyaudio comtypes numpy pygame pytube yandex-music gtts psutil pystray pillow keyboard plyer pyautogui deep-translator PyQt6 pip install python-telegram-bot dotenv --no-warn-script-location 
-
-:: Копирование в автозагрузку
-copy "%~dp0FridayStart.bat" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\"
-
-echo Установка завершена! Пятница будет запускаться автоматически.
-pause
